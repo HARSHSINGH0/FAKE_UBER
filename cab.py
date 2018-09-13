4d5201b15a23b7678c2a346400c48977fa6b5b7e
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2018-09-13 18:58:43 +0530

    added otp feature

--- cab.py
+++ cab.py
@@ -9,17 +9,17 @@ car_wala3=["Name:Manoj singh,Ratings:4.82,Age:46"]
 print(car_wala1,car_wala2,car_wala3)
 print("Select any one")
 x=input()
-x-=1
-if x==0:
-    print("thanks for choosing"+car_wala1)
+
 if x==1:
+    print("thanks for choosing"+car_wala1)
+elif x==2:
     print("thanks for choosing"+car_wala2)
-if x==2:
+elif x==3:
     print("thanks for choosing"+car_wala3)
 
 
 
-print(car_wala)
+
 print("HI SIR WHAT'S YOUR NAME")
 a=input()
 customer_name=str(a)
@@ -29,4 +29,13 @@ print("what is destination sir")
 destination=input()
 print("THANKS SIR FOR SUBMITTING")
 print("HERE'S SOME INFORMATION ABOUT YOU:")
-print(customer_name," \n ",from_place," \n ",destination)
+print("Sir Your Name is ",customer_name," \n this is current location",from_place," \n this is your destination ",destination)
+otp=random.randrange(10000)
+print("this is otp",otp)
+
+print("Please Confirm your otp")
+car_otp=int(input())
+if otp==car_otp:
+    print("continue your ride")
+else:
+    print("re enter the otp")
